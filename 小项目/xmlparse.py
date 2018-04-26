507e009388c32182ea78f918399b63b361fcc92f
unknown <KMSJ000067@kangmei.com>
2018-04-26 17:33:05 +0800

    更新

---
+++ 小项目/xmlparse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: anchen
+# @Date:   2018-04-25 14:29:25
+# @Last Modified by:   anchen
+# @Last Modified time: 2018-04-26 10:54:34
+
+import  xml.etree.ElementTree as ET
+
+tree = ET.parse('sitemap-11.xml')
+root = tree.getroot()
+
+f = open('rs.txt','w')
+
+for loc in root.iter('loc'):
+    if loc.text.find('history_price',0)!=-1:
+        f.write(loc.text+'\n')
+f.close()
